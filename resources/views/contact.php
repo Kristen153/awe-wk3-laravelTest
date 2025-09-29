9e6b59aee613abe38c0d30a9b3f755d673b41512
Kristen153 <158039882+Kristen153@users.noreply.github.com>
2025-09-29 08:54:30 +0000

    about me and contacts test view

---
+++ resources/views/contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contacts Page Example</h1>
+    <h2>This is a sub heading<h2>
+    <ul>
+        <li>List item one</li>
+        <li>List item two</li>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-    //return 'hello world';
+});
+
+Route::get('/about', function () {
+    return view('about');
+    //return 'about page';
+    //return ['foo' => 'bar'];
+});
+
+Route::get('/contacts', function () {
+    return view('contact');
 });
